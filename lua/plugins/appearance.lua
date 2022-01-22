2a305e80629eb162f86f9b9b793cb1dc4c040d81
Xiaofeng Yuan <yuan.xiaofeng@gmail.com>
2022-01-22 21:46:13 +0800

    update indent-blankline's excluded filetypes and buftypes

--- lua/plugins/appearance.lua
+++ lua/plugins/appearance.lua
@@ -8,8 +8,8 @@ cmd "syntax on"
 g.indentLine_enabled = 1
 g.indent_blankline_char = "▏"
 
-g.indent_blankline_filetype_exclude = {"help", "terminal", "dashboard", "alpha"}
-g.indent_blankline_buftype_exclude = {"terminal"}
+g.indent_blankline_filetype_exclude = {"help", "terminal", "NvimTree", "TelescopePrompt", "dashboard", "alpha", ""}
+g.indent_blankline_buftype_exclude = {"terminal", "nofile"}
 
 g.indent_blankline_show_trailing_blankline_indent = false
 g.indent_blankline_show_first_indent_level = false
