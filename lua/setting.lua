5f10241a6c71cb0ada7f7786a1fbef0456958e2d
Xiaofeng Yuan <yuan.xiaofeng@gmail.com>
2022-04-03 19:41:58 +0800

    remove sessionoptions setting which seems not save the layout with auto-session plugin

--- lua/setting.lua
+++ lua/setting.lua
@@ -78,9 +78,6 @@ opt("o", "foldenable", false)
 opt("o", "foldmethod", 'indent')
 opt("o", "foldlevel", 1)
 
--- better setting for auto-session
-opt("o", "sessionoptions", "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal")
-
 -- ================= Useful ================= --
 -- remove trailing whitespaces
 cmd([[autocmd BufWritePre * %s/\s\+$//e]])
