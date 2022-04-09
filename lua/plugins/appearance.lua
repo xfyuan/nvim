dc5b8e53de764f39a9a3a00a99c1463525926835
Xiaofeng Yuan <yuan.xiaofeng@gmail.com>
2022-04-09 23:48:42 +0800

    remove no used setting for `hide line numbers , statusline in specific buffers`

--- lua/plugins/appearance.lua
+++ lua/plugins/appearance.lua
@@ -14,15 +14,7 @@ require('nightfox').setup {
 -- cmd('colorscheme tokyonight')
 cmd('colorscheme everforest')
 
--- hide line numbers , statusline in specific buffers!
-vim.api.nvim_exec(
-    [[
-   au BufEnter term://* setlocal nonumber
-   au BufEnter,BufWinEnter,WinEnter,CmdwinEnter * if bufname('%') == "NvimTree" | set laststatus=0 | else | set laststatus=2 | endif
-   au BufEnter term://* set laststatus=0
-]],
-    false
-)
+-- ================= Plugins ================= --
 
 -- indent-blankline.nvim
 require("indent_blankline").setup {
