68f8f8f2668caf4f2829230e004e8bef592bb478
Xiaofeng Yuan <yuan.xiaofeng@gmail.com>
2022-12-12 17:33:07 +0800

    try to use max397574/better-escape.nvim

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -24,7 +24,7 @@ map("v", "x", [=[ "_x ]=], opt)
 
 -- ================= Core Powerfull Mapping ================= -- {{{
 
-cmd("inoremap jj <Esc>")
+-- cmd("inoremap jj <Esc>")
 cmd("tnoremap jj <C-\\><C-n>")
 
 -- general --
