4e3b4c22af022ced4a6f3e6282aae576d51ef190
Xiaofeng Yuan <yuan.xiaofeng@gmail.com>
2023-01-24 22:45:10 +0800

    change hlslens setting

--- lua/plugins/config/hlslens.lua
+++ lua/plugins/config/hlslens.lua
@@ -1,48 +1,15 @@
-local api = vim.api
-local keymap = vim.keymap
-
 local hlslens = require("hlslens")
 
-hlslens.setup {
+hlslens.setup({
   calm_down = true,
   nearest_only = true,
-}
-
-local activate_hlslens = function(direction)
-  local cmd = string.format("normal! %s%szzzv", vim.v.count1, direction)
-  local status, msg = pcall(vim.cmd, cmd)
-
-  if not status then
-    -- 13 is the index where real error message starts
-    msg = msg:sub(13)
-    api.nvim_err_writeln(msg)
-    return
-  end
-
-  hlslens.start()
-end
-
-keymap.set("n", "n", "", {
-  callback = function()
-    activate_hlslens("n")
-  end,
 })
 
-keymap.set("n", "N", "", {
-  callback = function()
-    activate_hlslens("N")
-  end,
-})
+local kopts = {noremap = true, silent = true}
 
-keymap.set("n", "*", "", {
-  callback = function()
-    vim.fn.execute("normal! *N")
-    hlslens.start()
-  end,
-})
-keymap.set("n", "#", "", {
-  callback = function()
-    vim.fn.execute("normal! #N")
-    hlslens.start()
-  end,
-})
+vim.api.nvim_set_keymap('n', 'n', [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]], kopts)
+vim.api.nvim_set_keymap('n', 'N', [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]], kopts)
+vim.api.nvim_set_keymap('n', '*', [[*<Cmd>lua require('hlslens').start()<CR>]], kopts)
+vim.api.nvim_set_keymap('n', '#', [[#<Cmd>lua require('hlslens').start()<CR>]], kopts)
+
+vim.api.nvim_set_keymap('n', '<Leader>l', '<Cmd>noh<CR>', kopts)
