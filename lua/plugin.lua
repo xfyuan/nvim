448dd09657f35c50e260b2b2ebad15089bdb5e27
Xiaofeng Yuan <yuan.xiaofeng@gmail.com>
2023-02-03 19:30:59 +0800

    add noice.nvim again, because the cursor issue disappear in cmd popup window has been fixed in tokyonight colorscheme

--- lua/plugin.lua
+++ lua/plugin.lua
@@ -78,6 +78,31 @@ require("lazy").setup({
     end,
   },
   {
+    "folke/noice.nvim",
+    dependencies = { "MunifTanjim/nui.nvim" },
+    event = "VeryLazy",
+    enabled = true,
+    opts = {
+      lsp = {
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+        signature = {
+          enabled = false,
+        }
+      },
+      presets = {
+        bottom_search = true, -- use a classic bottom cmdline for search
+        command_palette = true, -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = false, -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = false, -- add a border to hover docs and signature help
+      },
+    },
+  },
+  {
     "phaazon/hop.nvim", -- Neovim motions on speed! An EasyMotion-like plugin allowing you to jump anywhere in a document
     config = function()
       require("hop").setup()
