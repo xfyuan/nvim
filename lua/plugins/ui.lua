5d69e8f8c532b475dbf4063da0d4ed6fc2271b89
Xiaofeng Yuan <yuan.xiaofeng@gmail.com>
2023-02-08 19:49:25 +0800

    add more menu in the dashboard

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -270,12 +270,12 @@ return {
       local logo = require("util.logo")["random"]
       dashboard.section.header.val = vim.split(logo, "\n")
       dashboard.section.buttons.val = {
-        -- dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
+        dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
         dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
         dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
+        dashboard.button("g", " " .. " Grep text", ":Telescope live_grep <CR>"),
         dashboard.button("u", "鈴" .. " Update plugins", ":Lazy update<CR>"),
-        -- dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
-        -- dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
+        dashboard.button("c", " " .. " Config NeoVim", ":e $MYVIMRC <CR>"),
         dashboard.button("q", " " .. " Quit", ":qa<CR>"),
       }
       for _, button in ipairs(dashboard.section.buttons.val) do
