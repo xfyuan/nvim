446955124c393a34ec86a4dbb6b714205cd31499
Xiaofeng Yuan <yuan.xiaofeng@gmail.com>
2023-02-18 11:45:45 +0800

    ignore some cmd's auto cmp popup, such as `Git`

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -84,8 +84,13 @@ return {
         sources = cmp.config.sources({
           { name = "path" },
         }, {
-          { name = "cmdline" },
-        }),
+            {
+              name = "cmdline",
+              option = {
+                ignore_cmds = { 'Man', 'Git', '!' }
+              }
+            },
+          }),
       })
 
       return {
