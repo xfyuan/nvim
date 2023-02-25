9c65f289e1ac6d8cc5a132d67e007da5cd8ef3c3
Xiaofeng Yuan <yuan.xiaofeng@gmail.com>
2023-02-25 23:13:58 +0800

    add vim-matchup plugin for more comforatble matches

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -44,6 +44,13 @@ return {
     build = "cd app && yarn install",
     ft = "markdown",
   },
+  {
+    "andymass/vim-matchup",
+    event = { "BufReadPost" },
+    config = function()
+      vim.g.matchup_matchparen_offscreen = { method = "popup" }
+    end,
+  },
   --  +------------------------------------------------------------------------------+
   --  |                                     Git                                      |
   --  +------------------------------------------------------------------------------+
