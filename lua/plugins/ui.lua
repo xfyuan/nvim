1bd5283330271567e1ac8f5ef5e91eb3e05f96fc
Xiaofeng Yuan <yuan.xiaofeng@gmail.com>
2023-03-17 17:50:06 +0800

    reduce notify message display time from 3s to 1s

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -181,7 +181,7 @@ return {
       { "<leader>nD", function() require("notify").dismiss({ silent = true, pending = true }) end, desc = "Delete all Notifications", },
     },
     opts = {
-      timeout = 3000,
+      timeout = 1000,
       max_height = function()
         return math.floor(vim.o.lines * 0.75)
       end,
