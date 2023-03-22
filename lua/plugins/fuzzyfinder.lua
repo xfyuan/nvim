274a51bf2112a115215134eaccb48aaef58361f4
Xiaofeng Yuan <yuan.xiaofeng@gmail.com>
2023-03-22 19:40:38 +0800

    enable auto-session's new config `auto_session_use_git_branch`

--- lua/plugins/fuzzyfinder.lua
+++ lua/plugins/fuzzyfinder.lua
@@ -25,6 +25,7 @@ return {
           require("auto-session").setup({
             auto_session_root_dir = vim.fn.stdpath("config") .. "/sessions/",
             auto_session_enabled = false,
+            auto_session_use_git_branch = true,
           })
         end,
       },
