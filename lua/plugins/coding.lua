8c9706a193df95f41f0de8c09b75866049f84601
Xiaofeng Yuan <yuan.xiaofeng@gmail.com>
2023-05-08 19:13:31 +0800

    replace mini.surround with nvim-surround plugin, my familiar operation is coming!!

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -24,45 +24,20 @@ return {
     },
     config = true,
   },
+  -- surround
+  {
+    "kylechui/nvim-surround",
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup()
+    end
+  },
   -- auto pairs
   {
     "echasnovski/mini.pairs",
     event = "VeryLazy",
     config = function(_, opts)
       require("mini.pairs").setup(opts)
-    end,
-  },
-  -- surround
-  {
-    "echasnovski/mini.surround",
-    keys = function(plugin, keys)
-      -- Populate the keys based on the user's options
-      local opts = require("lazy.core.plugin").values(plugin, "opts", false)
-      local mappings = {
-        { opts.mappings.add, desc = "Add surrounding", mode = { "n", "v" } },
-        { opts.mappings.delete, desc = "Delete surrounding" },
-        { opts.mappings.find, desc = "Find right surrounding" },
-        { opts.mappings.find_left, desc = "Find left surrounding" },
-        { opts.mappings.highlight, desc = "Highlight surrounding" },
-        { opts.mappings.replace, desc = "Replace surrounding" },
-        { opts.mappings.update_n_lines, desc = "Update `MiniSurround.config.n_lines`" },
-      }
-      return vim.list_extend(mappings, keys)
-    end,
-    opts = {
-      mappings = {
-        add = "gza", -- Add surrounding in Normal and Visual modes
-        delete = "gzd", -- Delete surrounding
-        find = "gzf", -- Find surrounding (to the right)
-        find_left = "gzF", -- Find surrounding (to the left)
-        highlight = "gzh", -- Highlight surrounding
-        replace = "gzr", -- Replace surrounding
-        update_n_lines = "gzn", -- Update `n_lines`
-      },
-    },
-    config = function(_, opts)
-      -- use gz mappings instead of s to prevent conflict with leap
-      require("mini.surround").setup(opts)
     end,
   },
   -- better text-objects
