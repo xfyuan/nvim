cad760284a11277e5c6e176829b75ca53dc32818
Xiaofeng Yuan <yuan.xiaofeng@gmail.com>
2023-06-20 09:46:08 +0800

    updates for ui plugins config

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -9,13 +9,19 @@ return {
   -- better vim.ui
   {
     "stevearc/dressing.nvim",
-    event = "VeryLazy",
-    opts = {
-      input = { relative = "editor" },
-      select = {
-        backend = { "telescope", "fzf", "builtin" },
-      },
-    },
+    lazy = true,
+    init = function()
+      ---@diagnostic disable-next-line: duplicate-set-field
+      vim.ui.select = function(...)
+        require("lazy").load({ plugins = { "dressing.nvim" } })
+        return vim.ui.select(...)
+      end
+      ---@diagnostic disable-next-line: duplicate-set-field
+      vim.ui.input = function(...)
+        require("lazy").load({ plugins = { "dressing.nvim" } })
+        return vim.ui.input(...)
+      end
+    end,
   },
   --  +------------------------------------------------------------------------------+
   --  |                                  Indentline                                  |
@@ -23,10 +29,10 @@ return {
   -- indent guides for Neovim
   {
     "lukas-reineke/indent-blankline.nvim",
-    event = "BufReadPost",
+    event = { "BufReadPost", "BufNewFile" },
     opts = {
       char = "│",
-      filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
+      filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason", "notify", "toggleterm", },
       show_trailing_blankline_indent = false,
       show_current_context = false,
     },
@@ -35,19 +41,18 @@ return {
   {
     "echasnovski/mini.indentscope",
     version = false, -- wait till new 0.7.0 release to put it back on semver
-    event = "BufReadPre",
+    event = { "BufReadPre", "BufNewFile" },
     opts = {
       symbol = "│",
       options = { try_as_border = true },
     },
     config = function(_, opts)
       vim.api.nvim_create_autocmd("FileType", {
-        pattern = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
+        pattern = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason", "notify", "toggleterm" },
         callback = function()
           vim.b.miniindentscope_disable = true
         end,
       })
-      require("mini.indentscope").setup(opts)
     end,
   },
   --  +------------------------------------------------------------------------------+
@@ -90,31 +95,19 @@ return {
             { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
             { "filename", path = 1, symbols = { modified = "  ", readonly = "", unnamed = "" } },
             {
-              function()
-                return require("nvim-navic").get_location()
-              end,
-              cond = function()
-                return package.loaded["nvim-navic"] and require("nvim-navic").is_available()
-              end,
+              function() return require("nvim-navic").get_location() end,
+              cond = function() return package.loaded["nvim-navic"] and require("nvim-navic").is_available() end,
             },
           },
           lualine_x = {
             {
-              function()
-                return require("noice").api.status.command.get()
-              end,
-              cond = function()
-                return package.loaded["noice"] and require("noice").api.status.command.has()
-              end,
+              function() return require("noice").api.status.command.get() end,
+              cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
               color = fg("Statement"),
             },
             {
-              function()
-                return require("noice").api.status.mode.get()
-              end,
-              cond = function()
-                return package.loaded["noice"] and require("noice").api.status.mode.has()
-              end,
+              function() return require("noice").api.status.mode.get() end,
+              cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
               color = fg("Constant"),
             },
             { require("lazy.status").updates, cond = require("lazy.status").has_updates, color = fg("Special") },
@@ -146,7 +139,7 @@ return {
           lualine_y = {},
           lualine_z = {},
         },
-        extensions = { "nvim-tree", "quickfix", "fugitive", "aerial", "toggleterm" },
+        extensions = { "neo-tree", "lazy", "quickfix", "fugitive", "aerial", "toggleterm" },
       }
     end,
   },
@@ -199,12 +192,27 @@ return {
         override = {
           ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
           ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+      },
+      routes = {
+        {
+          filter = {
+            event = "msg_show",
+            any = {
+              { find = "%d+L, %d+B" },
+              { find = "; after #%d+" },
+              { find = "; before #%d+" },
+            },
+          },
+          view = "mini",
         },
       },
       presets = {
         bottom_search = true,
         command_palette = true,
         long_message_to_split = true,
+        inc_rename = true,
         lsp_doc_border = true,
       },
     },
@@ -214,6 +222,7 @@ return {
       { "<leader>nl", function() require("noice").cmd("last") end, desc = "Noice Last Message", },
       { "<leader>nh", function() require("noice").cmd("history") end, desc = "Noice History", },
       { "<leader>na", function() require("noice").cmd("all") end, desc = "Noice All", },
+      { "<leader>nd", function() require("noice").cmd("dismiss") end, desc = "Dismiss All" },
       {
         "<c-f>",
         function()
@@ -271,7 +280,7 @@ return {
       local logo = require("util.logo")["random"]
       dashboard.section.header.val = vim.split(logo, "\n")
       dashboard.section.buttons.val = {
-        dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
+        dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
         dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
         dashboard.button("r", " " .. " Recent files", ":Telescope frecency <CR>"),
         -- dashboard.button("g", " " .. " Grep text", ":Telescope live_grep <CR>"),
