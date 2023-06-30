2a4774071adba950f27af5bd02dca94329b5b88e
Xiaofeng Yuan <yuan.xiaofeng@gmail.com>
2023-06-30 18:47:10 +0800

    set telescope.nvim to a special commit `219584a6` version,
    the latest commit broken the `g]` to display the ctags list

--- lua/plugins/fuzzyfinder.lua
+++ lua/plugins/fuzzyfinder.lua
@@ -5,7 +5,7 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     cmd = "Telescope",
-    version = false, -- telescope did only one release, so use HEAD for now
+    commit = '219584a6', -- telescope did only one release, so use HEAD for now
     dependencies = {
       { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
       { "nvim-telescope/telescope-symbols.nvim" },
