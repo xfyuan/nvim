da436c640555f3cf819a0a7adb7e6c9580d1a423
Xiaofeng Yuan <yuan.xiaofeng@gmail.com>
2023-07-06 14:42:35 +0800

    add a hotkey `p` to open project in the alpha screen

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -283,6 +283,7 @@ return {
         dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
         dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
         dashboard.button("r", " " .. " Recent files", ":Telescope frecency <CR>"),
+        dashboard.button("p", " " .. " Projects", ":lua require'telescope'.extensions.project.project{}<CR>"),
         -- dashboard.button("g", " " .. " Grep text", ":Telescope live_grep <CR>"),
         dashboard.button("u", "鈴" .. " Update plugins", ":Lazy update<CR>"),
         dashboard.button("c", " " .. " Config NeoVim", ":e $MYVIMRC <CR>"),
