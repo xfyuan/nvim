f61bb1d3afdcf5e84faeb93abfa9bef605386e4a
Xiaofeng Yuan <yuan.xiaofeng@gmail.com>
2023-07-13 21:40:02 +0800

    disable solargraph again because it's too slow and often 99% CPU cost in large project!!

--- lua/plugins/extras/lang/ruby.lua
+++ lua/plugins/extras/lang/ruby.lua
@@ -5,32 +5,32 @@ return {
       vim.list_extend(opts.ensure_installed, { "ruby", })
     end,
   },
-  {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, { "solargraph", })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        solargraph = {
-          -- root_dir = require("lspconfig.util").root_pattern(".git", "Gemfile"),
-          settings = {
-            solargraph = {
-              completion = true,
-              symbols = true,
-              diagnostics = true,
-              definitions = true,
-              hover = true,
-              references = true,
-              rename = true,
-              useBundler = true,
-            },
-          },
-        },
-      },
-    },
-  },
+  -- {
+  --   "williamboman/mason.nvim",
+  --   opts = function(_, opts)
+  --     vim.list_extend(opts.ensure_installed, { "solargraph", })
+  --   end,
+  -- },
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   opts = {
+  --     servers = {
+  --       solargraph = {
+  --         -- root_dir = require("lspconfig.util").root_pattern(".git", "Gemfile"),
+  --         settings = {
+  --           solargraph = {
+  --             completion = true,
+  --             symbols = true,
+  --             diagnostics = true,
+  --             definitions = true,
+  --             hover = true,
+  --             references = true,
+  --             rename = true,
+  --             useBundler = true,
+  --           },
+  --         },
+  --       },
+  --     },
+  --   },
+  -- },
 }
