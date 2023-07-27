56fdcf6abeddecc9441bf3faf5763ad0477ca07b
Xiaofeng Yuan <yuan.xiaofeng@gmail.com>
2023-07-27 18:37:30 +0800

    add autocmd to disable Focus for certain buffer or file types

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -99,6 +99,32 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
     vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
   end,
 })
+
+-- Disable Focus for certain buffer or file types
+local ignore_filetypes = { 'neo-tree' }
+local ignore_buftypes = { 'nofile', 'prompt', 'popup' }
+local augroup =
+    vim.api.nvim_create_augroup('FocusDisable', { clear = true })
+
+vim.api.nvim_create_autocmd('WinEnter', {
+  group = augroup,
+  callback = function(_)
+    if vim.tbl_contains(ignore_buftypes, vim.bo.buftype) then
+      vim.b.focus_disable = true
+    end
+  end,
+  desc = 'Disable focus autoresize for BufType',
+})
+vim.api.nvim_create_autocmd('FileType', {
+  group = augroup,
+  callback = function(_)
+    if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
+      vim.b.focus_disable = true
+    end
+  end,
+  desc = 'Disable focus autoresize for FileType',
+})
+
 -- auto close neovim when nvim-tree is the last window
 -- vim.api.nvim_create_autocmd("BufEnter", {
 --   group = vim.api.nvim_create_augroup("NvimTreeClose", { clear = true }),
