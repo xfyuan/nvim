eee86f7467f3cdc9ffc42cf9a451a97a0a4b6882
Xiaofeng Yuan <yuan.xiaofeng@gmail.com>
2024-01-29 20:53:52 +0800

    use neovide_transparency to replace neovide_background_color which is deprecated

--- lua/config/neovide.lua
+++ lua/config/neovide.lua
@@ -10,5 +10,5 @@ if vim.g.neovide then
   -- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
   vim.g.neovide_transparency = 0.8
   vim.g.transparency = 0.8
-  vim.g.neovide_background_color = "#0f1117" .. alpha()
+  vim.g.neovide_transparency = "#0f1117" .. alpha()
 end
