3ae4a04727cdf77a211225e5f7dfd1efa073a461
Xiaofeng Yuan <yuan.xiaofeng@gmail.com>
2024-03-20 14:40:16 +0800

    remove go-nvim dependency plugin which never be used

--- lua/plugins/extras/tool/go-nvim.lua
+++ lua/plugins/extras/tool/go-nvim.lua
@@ -1,10 +1,6 @@
 return {
   {
     "ray-x/go.nvim",
-    dependencies = {
-      "ray-x/guihua.lua",
-    },
-    event = { "CmdlineEnter" },
     ft = { "go", "gomod" },
     build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
     config = function()
