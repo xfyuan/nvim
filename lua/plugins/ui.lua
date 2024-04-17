fc88ca61df2bc95abc733b45b59142ac3a835ff6
Xiaofeng Yuan <yuan.xiaofeng@gmail.com>
2024-04-17 18:38:50 +0800

    update noice config to set search bar to center position

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -215,7 +215,7 @@ return {
         },
       },
       presets = {
-        bottom_search = true,
+        bottom_search = false,
         command_palette = true,
         long_message_to_split = true,
         inc_rename = true,
