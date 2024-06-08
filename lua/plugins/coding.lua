84a8e0b67aff2c77a3bf80bfcbb7e64bb413807e
Xiaofeng Yuan <yuan.xiaofeng@gmail.com>
2024-06-08 11:39:05 +0800

    update a bit aerial.nvim config

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -150,12 +150,15 @@ return {
     "stevearc/aerial.nvim",
     event = "User AstroFile",
     keys = {
-      { "<leader>co", "<cmd>AerialToggle<cr>", desc = "Toggle code outline window" },
+      { "<leader>o", "<cmd>AerialToggle<cr>", desc = "Toggle code outline window" },
     },
     opts = {
       attach_mode = "global",
       backends = { "lsp", "treesitter", "markdown", "man" },
       layout = { min_width = 28 },
+      manage_folds = true,
+      link_folds_to_tree = true,
+      link_tree_to_folds = true,
       show_guides = true,
       filter_kind = false,
       autojump = true,
