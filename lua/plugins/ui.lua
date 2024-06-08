582fb48d06c6518d57e15b6b42a9856126a46411
Xiaofeng Yuan <yuan.xiaofeng@gmail.com>
2024-06-08 11:30:51 +0800

    replace nvim-colorizer with nvim-highlight-colors which support the new virtual text feature of neovim 0.10

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -275,20 +275,12 @@ return {
   --  +------------------------------------------------------------------------------+
   -- high-performance color highlighter
   {
-    "norcalli/nvim-colorizer.lua",
+    "brenoprata10/nvim-highlight-colors",
     event = "VeryLazy",
-    config = function()
-      require("colorizer").setup({ "lua", "css", "scss", "erb", "html", "javascript" }, {
-        RGB = true, -- #RGB hex codes
-        RRGGBB = true, -- #RRGGBB hex codes
-        names = true, -- "Name" codes like Blue
-        RRGGBBAA = true, -- #RRGGBBAA hex codes
-        rgb_fn = true, -- CSS rgb() and rgba() functions
-        hsl_fn = true, -- CSS hsl() and hsla() functions
-        css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-        css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-      })
-    end,
+    opts = {
+      render = 'virtual',
+      enable_tailwind = true,
+    }
   },
   --  +------------------------------------------------------------------------------+
   --  |                                  Dashboard                                   |
