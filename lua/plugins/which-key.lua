609412a9fb46b61af65882ace09dc53a5245a826
Xiaofeng Yuan <yuan.xiaofeng@gmail.com>
2024-07-31 11:44:15 +0800

    change which-key config for its deprecated version(v3.0) update

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -4,104 +4,58 @@ return {
     "folke/which-key.nvim",
     event = "VeryLazy",
     opts = {
-      triggers_blacklist = {
-        -- list of mode / prefixes that should never be hooked by WhichKey
-        -- this is mostly relevant for key maps that start with a native binding
-        -- most people should not need to change this
-        i = { "i", "j", "k", "h", "u", "v" },
-        v = { "j", "k" },
-      },
+      -- triggers_blacklist = {
+      --   -- list of mode / prefixes that should never be hooked by WhichKey
+      --   -- this is mostly relevant for key maps that start with a native binding
+      --   -- most people should not need to change this
+      --   i = { "i", "j", "k", "h", "u", "v" },
+      --   v = { "j", "k" },
+      -- },
     },
     config = function(_, opts)
       local Util = require("util")
       local wk = require("which-key")
       wk.setup(opts)
 
-      local n_opts = { mode = "n", prefix = "<leader>" }
-      local v_opts = { mode = "v", prefix = "<leader>" }
-      local g_opts = { mode = "n", prefix = "g" }
-
       local normal_keymap = {
-        -- ['<leader>'] = {'<cmd>GitFiles<cr>', 'find git files'},
-        -- ['1'] = {':normal "lyy"lpwv$r=^"lyyk"lP<cr>', 'mark ======'},
-        q = { ":q!<cr>", "Quit without saving" },
-        Q = { ":qa!<cr>", "Quit all windows without saving" },
-        L = { "<cmd>Lazy<cr>", "open lazy.nvim plugins window" },
-        b = {
-          name = "Buffer",
-        },
-        c = {
-          name = "Coding",
-        },
-        d = {
-          name = "Diff",
-        },
-        f = {
-          name = "Fuzzy finder",
-        },
-        g = {
-          name = "Git",
-        },
-        h = {
-          name = "Git Hunk / Hurl",
-        },
-        j = {
-          name = "Easymotion",
-        },
-        l = {
-          name = "Url link / LSP",
-        },
-        m = {
-          name = "Marks / Markdown",
-        },
-        n = {
-          name = "Notification",
-        },
-        r = {
-          name = "Replace",
-        },
-        s = {
-          name = "Session",
-        },
-        t = {
-          name = "Test",
-        },
-        u = {
-          name = "Toggle option",
-          w = { function() Util.toggle("wrap") end, "Toggle Word Wrap", },
-          s = { function() Util.toggle("spell") end, "Toggle Spelling", },
-          n = { function() Util.toggle("relativenumber") end, "Toggle Line Numbers", },
-          d = { function() Util.toggle_diagnostics() end, "Toggle Diagnostics", },
-        },
-        w = {
-          name = "Window / Word",
-        },
-        x = {
-          name = "Trouble",
-        },
-      }
-
-      local visual_keymap = {
-        g = {
-          name = "Git",
-        },
-        r = {
-          name = "Refactor",
-        },
+        { "<leader>q", ":q!<cr>", desc = "Quit without saving" },
+        { "<leader>Q", ":qa!<cr>", desc = "Quit all windows without saving" },
+        { "<leader>L", "<cmd>Lazy<cr>", desc = "open lazy.nvim plugins window" },
+        { "<leader>b", group = "Buffer" },
+        { "<leader>c", group = "Coding" },
+        { "<leader>d", group = "Diff" },
+        { "<leader>f", group = "Fuzzy finder" },
+        { "<leader>g", group = "Git" },
+        { "<leader>h", group = "Git Hunk / Hurl" },
+        { "<leader>j", group = "Easymotion" },
+        { "<leader>l", group = "Url link / LSP" },
+        { "<leader>m", group = "Marks / Markdown" },
+        { "<leader>n", group = "Notification" },
+        { "<leader>r", group = "Replace" },
+        { "<leader>s", group = "Session" },
+        { "<leader>t", group = "Test" },
+        { "<leader>w", group = "Window / Word" },
+        { "<leader>x", group = "Trouble" },
+        { "<leader>u", group = "Toggle option" },
+        { "<leader>ud", function() Util.toggle_diagnostics() end, desc = "Toggle Diagnostics" },
+        { "<leader>un", function() Util.toggle("relativenumber") end, desc = "Toggle Line Numbers" },
+        { "<leader>us", function() Util.toggle("spell") end, desc = "Toggle Spelling" },
+        { "<leader>uw", function() Util.toggle("wrap") end, desc = "Toggle Word Wrap" },
       }
 
       local global_keymap = {
-        c = {
-          name = "Comment box",
-        },
-        o = {
-          name = "Go with go.nvim",
-        },
+        { "gc", group = "Comment box" },
+        { "go", group = "Go with go.nvim" },
       }
 
-      wk.register(normal_keymap, n_opts)
-      wk.register(visual_keymap, v_opts)
-      wk.register(global_keymap, g_opts)
+      local visual_keymap = {
+        { "<leader>g", group = "Git", mode = "v" },
+        { "<leader>r", group = "Refactor", mode = "v" },
+      }
+
+      wk.add(normal_keymap)
+      wk.add(global_keymap)
+      wk.add(visual_keymap)
     end,
   },
 }
