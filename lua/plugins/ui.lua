a3a077d463cd1935b21047ba0999b0a419e9761e
Xiaofeng Yuan <yuan.xiaofeng@gmail.com>
2024-10-01 17:58:29 +0800

    add mini.icons, remove nvim-web-devicons

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -2,8 +2,6 @@ return {
   --  +------------------------------------------------------------------------------+
   --  |                              Basic enhencement                               |
   --  +------------------------------------------------------------------------------+
-  -- icons
-  { "nvim-tree/nvim-web-devicons", lazy = true },
   -- ui components
   { "MunifTanjim/nui.nvim", lazy = true },
   -- better vim.ui
@@ -20,6 +18,26 @@ return {
       vim.ui.input = function(...)
         require("lazy").load({ plugins = { "dressing.nvim" } })
         return vim.ui.input(...)
+      end
+    end,
+  },
+  -- icons
+  {
+    "echasnovski/mini.icons",
+    lazy = true,
+    opts = {
+      file = {
+        [".keep"] = { glyph = "󰊢", hl = "MiniIconsGrey" },
+        ["devcontainer.json"] = { glyph = "", hl = "MiniIconsAzure" },
+      },
+      filetype = {
+        dotenv = { glyph = "", hl = "MiniIconsYellow" },
+      },
+    },
+    init = function()
+      package.preload["nvim-web-devicons"] = function()
+        require("mini.icons").mock_nvim_web_devicons()
+        return package.loaded["nvim-web-devicons"]
       end
     end,
   },
