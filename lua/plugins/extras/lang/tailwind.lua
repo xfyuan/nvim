d245774569bebedf40ae84f133376ceecb5c4f02
Xiaofeng Yuan <yuan.xiaofeng@gmail.com>
2024-10-08 18:25:46 +0800

    update tailwindcss lsp config

--- lua/plugins/extras/lang/tailwind.lua
+++ lua/plugins/extras/lang/tailwind.lua
@@ -9,7 +9,7 @@ return {
       },
       setup = {
         tailwindcss = function(_, opts)
-          local tw = require "lspconfig.server_configurations.tailwindcss"
+          local tw = require "lspconfig.configs.tailwindcss"
           --- @param ft string
           opts.filetypes = vim.tbl_filter(function(ft)
             return not vim.tbl_contains(opts.filetypes_exclude or {}, ft)
