6b30a7d09fead2564e42ec1e361e9490ff9cb055
Xiaofeng Yuan <yuan.xiaofeng@gmail.com>
2024-10-10 14:39:05 +0800

    update neovide config for its latest version

--- lua/config/neovide.lua
+++ lua/config/neovide.lua
@@ -3,12 +3,7 @@ if vim.g.neovide then
   vim.g.neovide_hide_mouse_when_typing = true
   vim.opt.mouse = "a"
 
-  -- Helper function for transparency formatting
-  local alpha = function()
-    return string.format("%x", math.floor(255 * vim.g.transparency or 0.8))
-  end
   -- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
   vim.g.neovide_transparency = 0.95
   vim.g.transparency = 0.8
-  vim.g.neovide_transparency = "#0f1117" .. alpha()
 end
