02ccc45ce7fb9b5ef5aca859b3fe2c4c98b8a103
Xiaofeng Yuan <yuan.xiaofeng@gmail.com>
2024-10-29 16:28:24 +0800

    update neovide to match its latest version, add `<D-c>/<D-v>` as copy/paste hotkeys

--- lua/config/neovide.lua
+++ lua/config/neovide.lua
@@ -2,8 +2,12 @@ if vim.g.neovide then
   vim.g.neovide_remember_window_size = true
   vim.g.neovide_hide_mouse_when_typing = true
   vim.opt.mouse = "a"
+  vim.opt.guifont = "JetBrainsMono Nerd Font Mono:h20"
 
-  -- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
   vim.g.neovide_transparency = 0.95
   vim.g.transparency = 0.8
+
+  vim.cmd('vnoremap <D-c> "*y')
+  vim.cmd('nnoremap <D-v> "*p')
+  vim.cmd('inoremap <D-v> <C-r>+')
 end
