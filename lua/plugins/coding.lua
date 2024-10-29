04efcc17443e055ed05c71479a08bbd7b4734607
Xiaofeng Yuan <yuan.xiaofeng@gmail.com>
2024-10-29 16:20:09 +0800

    add some mini.pair config

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -35,6 +35,15 @@ return {
     "echasnovski/mini.pairs",
     event = "InsertEnter",
     opts = {
+      -- skip autopair when next character is one of these
+      skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+      -- skip autopair when the cursor is inside these treesitter nodes
+      skip_ts = { "string" },
+      -- skip autopair when next character is closing pair
+      -- and there are more closing pairs than opening pairs
+      skip_unbalanced = true,
+      -- better deal with markdown code blocks
+      markdown = true,
       mappings = {
         ["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\`].", register = { cr = false } },
       },
