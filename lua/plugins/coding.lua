ff5e2f21b58c07026f3a07bf18d4bd42e2d97693
Xiaofeng Yuan <yuan.xiaofeng@gmail.com>
2024-10-29 16:20:52 +0800

    change keymap for todo finding

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -128,7 +128,7 @@ return {
     keys = {
       { "]t", function() require("todo-comments").jump_next() end, desc = "Next todo comment", },
       { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous todo comment", },
-      { "<leader>ft", "<cmd>TodoTelescope<cr>", desc = "Find Todo" },
+      { "<leader>fT", "<cmd>TodoTelescope<cr>", desc = "Find Todo" },
     },
   },
   --  +------------------------------------------------------------------------------+
