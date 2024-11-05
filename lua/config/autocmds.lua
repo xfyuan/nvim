eb193608d8252933929004a75008c3e6d1d00f24
Xiaofeng Yuan <yuan.xiaofeng@gmail.com>
2024-11-05 18:09:20 +0800

    remove autocmd for keeping cursor not at the bottom of the screen, just found it's not useful for me

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -99,22 +99,6 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   end,
 })
 
--- Keep your cursor not at the bottom of the screen
-vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI", "BufEnter" }, {
-  group = grp,
-  callback = function()
-    local win_h = vim.api.nvim_win_get_height(0)
-    local off = math.min(vim.o.scrolloff, math.floor(win_h / 2))
-    local dist = vim.fn.line "$" - vim.fn.line "."
-    local rem = vim.fn.line "w$" - vim.fn.line "w0" + 1
-    if dist < off and win_h - rem + dist < off then
-      local view = vim.fn.winsaveview()
-      view.topline = view.topline + off - (win_h - rem + dist)
-      vim.fn.winrestview(view)
-    end
-  end,
-})
-
 local ignore_filetypes = { 'neo-tree' }
 local ignore_buftypes = { 'nofile', 'prompt', 'popup' }
 
