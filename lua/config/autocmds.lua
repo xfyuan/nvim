0cb42c157ba01544f3da7d3103ceabcbc7666aaf
Xiaofeng Yuan <yuan.xiaofeng@gmail.com>
2024-11-18 11:22:26 +0800

    disable diagnostic for markdown

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -83,6 +83,7 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = { "gitcommit", "markdown" },
   callback = function()
     vim.opt_local.wrap = true
+    vim.diagnostic.enable(false)
     -- vim.opt_local.spell = true
   end,
 })
