835d91fa52e2489a7af2dfda31e60e45017dd11f
Xiaofeng Yuan <yuan.xiaofeng@gmail.com>
2025-03-03 17:41:08 +0800

    many neo-tree.nvim config updates

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -89,13 +89,6 @@ return {
     cmd = "Neotree",
     keys = {
       {
-        "<leader>E",
-        function()
-          require("neo-tree.command").execute({ toggle = true, dir = require("util").get_root() })
-        end,
-        desc = "Explorer NeoTree (root dir)",
-      },
-      {
         "<C-u>",
         function()
           require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
@@ -107,21 +100,31 @@ return {
       vim.cmd([[Neotree close]])
     end,
     init = function()
-      vim.g.neo_tree_remove_legacy_commands = 1
-      if vim.fn.argc() == 1 then
-        local stat = vim.loop.fs_stat(vim.fn.argv(0))
-        if stat and stat.type == "directory" then
-          require("neo-tree")
-        end
-      end
+      -- FIX: use `autocmd` for lazy-loading neo-tree instead of directly requiring it,
+      -- because `cwd` is not set up properly.
+      vim.api.nvim_create_autocmd("BufEnter", {
+        group = vim.api.nvim_create_augroup("Neotree_start_directory", { clear = true }),
+        desc = "Start Neo-tree with directory",
+        once = true,
+        callback = function()
+          if package.loaded["neo-tree"] then
+            return
+          else
+            local stats = vim.uv.fs_stat(vim.fn.argv(0))
+            if stats and stats.type == "directory" then
+              require("neo-tree")
+            end
+          end
+        end,
+      })
     end,
     opts = {
       open_files_do_not_replace_types = { "terminal", "Trouble", "qf", "Outline" },
       close_if_last_window = true,
       filesystem = {
         bind_to_cwd = false,
-        follow_current_file = true,
-        use_libuv_file_watcher = true
+        use_libuv_file_watcher = true,
+        follow_current_file = { enabled = true},
       },
       window = {
         mappings = {
