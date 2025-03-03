74e1a1b5e5e347691e79c88491c98220e339c908
Xiaofeng Yuan <yuan.xiaofeng@gmail.com>
2025-03-03 17:31:39 +0800

    mnay oil.nvim config updates

--- lua/plugins/extras/tool/oil.lua
+++ lua/plugins/extras/tool/oil.lua
@@ -1,6 +1,7 @@
 -- Declare a global function to retrieve the current directory
 function _G.get_oil_winbar()
-  local dir = require("oil").get_current_dir()
+  local bufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
+  local dir = require("oil").get_current_dir(bufnr)
   if dir then
     return vim.fn.fnamemodify(dir, ":~")
   else
@@ -16,7 +17,7 @@ return {
       { "SirZenith/oil-vcs-status" },
     },
     keys = {
-      { "<leader>e", "<cmd>Oil<cr>", mode = { "n" }, desc = "File explorer" },
+      { "<leader>e", "<cmd>Oil<cr>", mode = { "n" }, desc = "Oil explorer" },
     },
     opts = {
       use_default_keymaps = false,
@@ -27,22 +28,61 @@ return {
       },
       keymaps = {
         ["g?"] = "actions.show_help",
-        ["<CR>"] = "actions.select",
-        ["<C-s>"] = { "actions.select", opts = { vertical = true }, desc = "Open the entry in a vertical split" },
-        ["<C-h>"] = { "actions.select", opts = { horizontal = true }, desc = "Open the entry in a horizontal split" },
+        -- ["<CR>"] = "actions.select",
+        ["o"] = "actions.select",
+        ["<C-v>"] = { "actions.select", opts = { vertical = true }, desc = "Open the entry in a vertical split" },
+        ["<C-s>"] = { "actions.select", opts = { horizontal = true }, desc = "Open the entry in a horizontal split" },
         ["<C-t>"] = { "actions.select", opts = { tab = true }, desc = "Open the entry in new tab" },
         ["<C-p>"] = "actions.preview",
         ["q"] = "actions.close",
         ["<C-r>"] = "actions.refresh",
         ["<C-y>"] = "actions.yank_entry",
-        ["-"] = "actions.parent",
-        ["_"] = "actions.open_cwd",
-        ["`"] = "actions.cd",
-        ["~"] = { "actions.cd", opts = { scope = "tab" }, desc = ":tcd to the current oil directory" },
-        ["gs"] = "actions.change_sort",
+        -- ["-"] = { "actions.parent", mode = "n" },
+        ["U"] = { "actions.parent", mode = "n" },
+        ["_"] = { "actions.open_cwd", mode = "n" },
+        ["`"] = { "actions.cd", mode = "n" },
+        ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+        ["gS"] = "actions.change_sort",
         ["gx"] = "actions.open_external",
         ["g."] = "actions.toggle_hidden",
         ["g\\"] = "actions.toggle_trash",
+        -- copy relative path
+        ["gy"] = {
+          callback = function()
+            local oil = require "oil"
+            local entry = oil.get_cursor_entry()
+            local dir = oil.get_current_dir()
+
+            if not entry or not dir then
+              return
+            end
+
+            local relpath = vim.fn.fnamemodify(dir, ":.")
+            vim.fn.setreg("+", relpath .. entry.name)
+          end,
+        },
+        -- search and replace in the current directory
+        ["gs"] = {
+          callback = function()
+            local oil = require "oil"
+            -- get the current directory
+            local prefills = { paths = oil.get_current_dir() }
+            local grug_far = require "grug-far"
+            -- instance check
+            if not grug_far.has_instance "explorer" then
+              grug_far.open {
+                instanceName = "explorer",
+                prefills = prefills,
+                staticTitle = "Find and Replace from Explorer",
+              }
+            else
+              grug_far.open_instance "explorer"
+              -- updating the prefills without clearing the search and other fields
+              grug_far.update_instance_prefills("explorer", prefills, false)
+            end
+          end,
+          desc = "oil: Search in directory",
+        },
       },
     },
   },
